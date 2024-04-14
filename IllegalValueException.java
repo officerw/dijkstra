254c11ec1965864dbe5441fd84dc1295c72732f9
William Officer <97336340+officerw@users.noreply.github.com>
2024-04-14 00:03:07 -0500

    Added files from project

---
+++ IllegalValueException.java
@@ -0,0 +1,20 @@
+package dijkstra;
+//package weiss.nonstandard;
+
+/**
+ * Exception class for illegal decrease key
+ * operations in pairing heaps.
+ * @author Mark Allen Weiss
+ */
+
+public class IllegalValueException extends RuntimeException
+{
+    /**
+     * Construct this exception object.
+     * @param message the error message.
+     */
+    public IllegalValueException( String message )
+    {
+        super( message );
+    }
+}
---
+++ UnderflowException.java
@@ -0,0 +1,20 @@
+
+package dijkstra;
+//package weiss.nonstandard;
+
+/**
+ * Exception class for access in empty containers
+ * such as stacks, queues, and priority queues.
+ * @author Mark Allen Weiss
+ */
+public class UnderflowException extends RuntimeException
+{
+    /**
+     * Construct this exception object.
+     * @param message the error message.
+     */
+    public UnderflowException( String message )
+    {
+        super( message );
+    }
+}
